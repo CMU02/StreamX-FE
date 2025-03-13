3a8bb1cb7d4c19c9021ce3729d9914301a48cb18
choisiheon <0328ks@gmail.com>
2025-03-13 17:54:26 +0900

    Test : 테스트용 간단한 상단 바 제작

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,8 +9,23 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center bg-white">
-      <h1>Hello World</h1>
-    </div>
+      <div className="flex bg-F6F4F4">
+        <div>
+          <div className="flex bg-white px-4 py-5 items-center w-screen shadow-lg ">
+            <div className="flex items-center basis-1/3 ">
+              <div className="font-extrabold pl-4">
+                logoicon
+              </div>
+            </div>
+            <div className="flex justify-end basis-1/3">
+            </div>
+            <div className="flex basis-1/3 justify-end font-lightbold">
+              <div className="px-2 cursor-pointer hover:text-blue-500 active:text-blue-500"> 로그인</div>
+              <div className="px-2 cursor-pointer hover:text-blue-500 active:text-blue-500"> 출품하기</div>
+              <div className="px-2 cursor-pointer hover:text-blue-500 active:text-blue-500"> 장바구니</div>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
--- app/routes/hello.tsx
+++ app/routes/hello.tsx
@@ -1,4 +1,3 @@
-
 export default function Hello() {
     return (
         <div>
