2019410af9c08d8121b76640e41602873cc7cc80
HyeonJun <hyeonjun1968@naver.com>
2025-06-01 19:38:38 +0900

    chore : project dependencies version update
    - tailwind : ^3.4.4 => ^4.1.8
    - 나머지 라이브러리 버전 업데이트
    - tailwind 버전에 맞게 설정 파일 구성
    - font 추가 (나눔 스퀘어 네오)

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,15 +2,23 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "StreamX" },
+    { name: "description", content: "Welcome to StreamX" },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center bg-white">
-      <h1>Hello World</h1>
+    <div className="flex h-screen items-center justify-center bg-white font-nanum-square-neo">
+      <div className="text-4xl">
+        <p className="font-[300]">안녕하세요. Hello world</p>
+        <p className="font-[400]">안녕하세요. Hello world</p>
+        <p className="font-[500]">안녕하세요. Hello world</p>
+        <p className="font-[600]">안녕하세요. Hello world</p>
+        <p className="font-[700]">안녕하세요. Hello world</p>
+        <p className="font-[800]">안녕하세요. Hello world</p>
+        <p className="font-[900]">안녕하세요. Hello world</p>
+      </div>
     </div>
   );
 }
